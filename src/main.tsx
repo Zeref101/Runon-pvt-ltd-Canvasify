c2e5cc0ab61868973df64f94d6cb4ec5c06ae0b4
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-01 22:24:02 +0530

    Theme Provider wrap around app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Draggable from 'react-draggable';
 import './index.css'
+import { ThemeProvider } from './context/ThemeProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Draggable>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Draggable>
   </React.StrictMode>,
 )
