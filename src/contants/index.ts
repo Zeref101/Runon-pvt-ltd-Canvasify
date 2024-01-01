b3537b2dc83fb3292251f28f9d544b0abe506e19
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-01 22:24:55 +0530

    theme constant added

---
+++ src/contants/index.ts
@@ -0,0 +1,5 @@
+export const themes = [
+  { value: "light", label: "Light", icon: "src/assets/icons/sun.svg" },
+  { value: "dark", label: "Dark", icon: "src/assets/icons/moon.svg" },
+  { value: "system", label: "System", icon: "src/assets/icons/computer.svg" },
+];
